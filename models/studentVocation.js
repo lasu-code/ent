014ae236b4706050012053357d491bcf3aa27f99
Olabayo Segun Emmanuel <hollasheg@gmail.com>
2018-11-03 10:54:37 -0400

    database connected

---
+++ models/studentVocation.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/adminSignup/studentvocation');
+
+const Schema = mongoose.Schema;
+
+let TweetSchema = new Schema ({
+
+		vocations: String,
+        matrics: String,
+        
+
+    createdDate:{
+        type: Date,
+        default: Date.now
+    }
+})
+module.exports = mongoose.model('studentvocation', TweetSchema);
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+let Lasu = require('../models/adminSignup');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -14,6 +15,10 @@ router.get('/login', function (req, res, next) {
     });
 });
 
+router.post('/login', function (req, res, next) {
+    res.redirect('/admin/categories');
+});
+
 router.get('/signup', function (req, res, next) {
     res.render('admin/signup', {
         title: 'Admin Signup',
@@ -21,8 +26,26 @@ router.get('/signup', function (req, res, next) {
     });
 });
 
+router.post('/signup', function (req, res, next) {
+    // res.render('admin/dashboard');
+    console.log(req.body);
+
+    let Admins = {
+      usernames: req.body.surname,
+      staffIds: req.body.staffId,
+      PhoneNumbers: req.body.phone,
+      StaffEmails: req.body.email,
+      
+    }
+    let data = new Lasu(Admins);
+
+    data.save();
+
+    res.redirect('/admin/categories');
+});
+
 router.get('/dashboard', function (req, res, next) {
-    res.render('admin/dashboard', {
+    res.render('admin/Dashboard', {
         title: 'Admin Dashboard'
     });
 });
